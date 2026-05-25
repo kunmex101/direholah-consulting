e175a512a33613d8f8bea3812ccffe96866aa21e
kunmex101 <65999411+kunmex101@users.noreply.github.com>
2026-05-25 13:45:14 -0400

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,1 +1,10 @@
-
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.direholahconsulting.com",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}
